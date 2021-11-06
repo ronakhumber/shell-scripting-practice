56913a75bbfc227c09395bec94444520d9b31915
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-11-06 11:35:05 +0530

    Displaying messages

--- calc.sh
+++ calc.sh
@@ -1,10 +1,21 @@
 #! /usr/bin/env bash
 
 # When the program is first loaded, display a greeting to the user.
+echo "Hello, this is Maysong Calculator. I simplify calculations for you."
+
 # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
   # Exit
+echo "The possible operations are:"
+  # Add
+echo "1 : Add"
+  # Subtract
+echo "2 : Substract"
+  # Multiply 
+echo "3 : Multiply"
+  # Exit
+echo "4 : Exit"
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
