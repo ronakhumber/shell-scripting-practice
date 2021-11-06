d61d6c688c798c48077960aef2947ba2e8e38b59
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-11-06 11:55:29 +0530

    Taking user input

--- calc.sh
+++ calc.sh
@@ -17,6 +17,9 @@ echo "3 : Multiply"
   # Exit
 echo "4 : Exit"
 # Then, capture the user selection.
+echo "Please enter the operation:"
+read selection
+echo "The operation entered was: $selection"
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
